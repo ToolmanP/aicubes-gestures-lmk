306525a6daf7728304e40170f29ef673e4187c30
toolmanp <toolmanp@outlook.com>
2022-08-20 01:35:22 +0800

    Update loss.py for both landmarks + heatmaps

--- glmk/loss.py
+++ glmk/loss.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as f
 import torch
 
 class FingerLoss(nn.Module):
@@ -6,9 +7,10 @@ class FingerLoss(nn.Module):
         super().__init__()
         self.entropy = nn.CrossEntropyLoss()
         self.mse = nn.MSELoss()
-    def forward(self, gesture, landmarks,gesture_gt, landmark_gt):
+
+    def forward(self, gesture, landmarks, heatmaps, gesture_gt, landmarks_gt, heatmap_gt):
         
         category_loss = self.entropy(gesture, gesture_gt)
-        mse_loss = nn.functional.mse_loss(landmarks,landmark_gt)
-
-        return mse_loss*category_loss
+        landmark_loss = f.mse_loss(landmarks,landmarks_gt)
+        heatmap_loss = sum([f.mse_loss(heatmap,heatmap_gt) for heatmap in heatmaps])/len(heatmaps)
+        return (0.5*landmark_loss+0.5*heatmap_loss)*category_loss
